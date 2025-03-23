2e4cf6ad3915bdce6117229ab353646e9423c8a9
Rafael <sharinzora10@gmail.com>
2025-03-23 00:43:46 -0600

    Controllers

---
+++ src/main/java/com/example/proyecto/Controllers/aboutController.java
@@ -0,0 +1,10 @@
+package com.example.proyecto.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class aboutController {
+    @GetMapping("/about")
+    public String about() { return "about"; }
+}
---
+++ src/main/java/com/example/proyecto/Services/userService.java
@@ -0,0 +1,8 @@
+package com.example.proyecto.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class userService {
+    //Creo que esto se conecta con la base de datos, porque lo de repo era para guardar cosas en el ejemplo
+}
